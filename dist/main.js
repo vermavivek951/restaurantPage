1f490567e6e9981df2431237595493c8a5f1dd64
Vivek Verma <vermavivek951@gmail.com>
2021-08-31 11:21:48 +0530

    Basic skelaton setup of Restaurant project

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+console.log("This basic setup works smoothly"),console.log("Okay, webpack running smoothly!!");
---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log("This basic setup works smoothly");
+console.log("Okay, webpack running smoothly!!");
